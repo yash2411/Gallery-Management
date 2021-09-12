8506121c7a89cef474e7f4f312d1a105dec68d76
Yash <yashpandey1106@gmail.com>
2021-09-12 13:30:06 +0530

    change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,8 +77,8 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:     'coderyash11@gmail.com',
-    password:      'Demat@1243',
+    user_name:     '',
+    password:      ''
     domain:        'localhost',
     address:       'smtp.gmail.com',
     port:          '587',
