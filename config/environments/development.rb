04124271e0a85d64a49b3bcf15e051dcea37d524
Yash <yashpandey1106@gmail.com>
2021-09-13 17:05:05 +0530

    H

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    user_name:     'coderyash11@gmail.com',
-    password:      'Demat@1243',
+    user_name:     '',
+    password:     '',
     domain:        'localhost',
     address:       'smtp.gmail.com',
     port:          '587',
