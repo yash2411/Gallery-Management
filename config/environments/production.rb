e264507e22174afcfb0a6372d1627a5b447df7e5
Yash <yashpandey1106@gmail.com>
2021-09-14 17:09:06 +0530

    h

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,10 +118,11 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = {:host => 'helloyash.herokuapp.com', :protocol => 'http'}
-  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:     'coderyash11@gmail.com',
-    password:     'Demat@1243',
+    password:      'Demat@1243',
     domain:        'helloyash.herokuapp.com',
     address:       'smtp.gmail.com',
     port:          '587',
