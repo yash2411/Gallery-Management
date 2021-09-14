df8d0d8054e59062f0bb8114a4363559f38bb9b3
Yash <yashpandey1106@gmail.com>
2021-09-14 17:06:06 +0530

    h

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,10 +117,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = {host: 'helloyash.herokuapp.com'}
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = {:host => 'helloyash.herokuapp.com', :protocol => 'http'}
+  
   config.action_mailer.smtp_settings = {
     user_name:     'coderyash11@gmail.com',
     password:     'Demat@1243',
