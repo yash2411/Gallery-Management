ccf07e7c567f5960a1202fec5f5cfb96b64c45b7
Yash <yashpandey1106@gmail.com>
2021-09-14 13:18:34 +0530

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   has_many :posts
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 end
