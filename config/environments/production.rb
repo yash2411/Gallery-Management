51e1ad25aa9ff3f970611bd7db1676f46abeb44c
Yash <yashpandey1106@gmail.com>
2021-09-14 17:23:14 +0530

    Ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  config.require_master_key = true
+  # config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -117,15 +117,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = {:host => 'helloyash.herokuapp.com', :protocol => 'http'}
-  config.action_mailer.perform_deliveries = true
+  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:     'coderyash11@gmail.com',
-    password:      'Demat@1243',
-    domain:        'helloyash.herokuapp.com',
+  host='helloyash.herokuapp.com'
+  config.action_mailer.default_url_options = {:host => host}
+  ActionMailer::Base.smtp_settings={
+    user_name:     ENV['SENDGRID_USERNAME'],
+    password:      ENV['SENDGRID_PASSWORD'],
+    domain:        'heroku.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
     enable_starttls_auto: true
+  }
+
 end
