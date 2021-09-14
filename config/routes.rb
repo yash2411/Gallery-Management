09a13eca18314ea4ea8d55ef9657431019eb1e01
Yash <yashpandey1106@gmail.com>
2021-09-14 13:55:03 +0530

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
   root 'posts#index'
-  get 'pages/about'
-  get 'pages/contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   get 'posts/show_post'
 
