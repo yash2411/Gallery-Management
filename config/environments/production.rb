c046ef9bf50d34bf0dcc4844eb1594535075edb5
Yash <yashpandey1106@gmail.com>
2021-09-16 10:17:20 +0530

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,8 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = {:host => 'yashgallery.herokuapp.com', :protocol=>'https'}
-
+  config.action_mailer.default_url_options = {:host => 'yashgallery.herokuapp.com', :protocol=>'http'}
   config.action_mailer.smtp_settings = {
    :address => "smtp.gmail.com",
    :port => 587,
