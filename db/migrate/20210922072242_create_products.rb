fee26dc433d77be67f041ef825a36c4739a5a9c2
Yash <yashpandey1106@gmail.com>
2021-09-29 11:55:56 +0530

    Added validations to product table

--- db/migrate/20210922072242_create_products.rb
+++ db/migrate/20210922072242_create_products.rb
@@ -3,7 +3,7 @@ class CreateProducts < ActiveRecord::Migration[6.1]
     create_table :products do |t|
       t.string :name
       t.integer :price
-      t.string :description
+      t.text :description
       t.integer :user_id
 
       t.timestamps
